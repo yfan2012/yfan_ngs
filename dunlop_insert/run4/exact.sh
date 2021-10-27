0dcdb0d17b461a6919f0b43a5fe4ba1972994509
yfan2012 <yfan2012@gmail.com>
2021-10-27 17:25:10 -0400

    analysis of gel extraction runs

---
+++ dunlop_insert/run4/exact.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+projdir=/mithril/Data/NGS/projects/dunlop_insert
+datadir=$projdir/run4
+ref1=$projdir/refs/construct1.fa
+ref2=$projdir/refs/construct2.fa
+
+
+if [ $1 == findmag ] ; then
+    mkdir -p $datadir/exact
+    for i in NT350 NT351 NT358 NT359 ;
+    do
+	(echo $i
+	python ~/Code/yfan_ngs/dunlop_insert/find_exact.py \
+           -1 $datadir/trimmed/${i}_fwd_paired.fq.gz \
+           -2 $datadir/trimmed/${i}_rev_paired.fq.gz \
+           -3 $datadir/trimmed/${i}_fwd_unpaired.fq.gz \
+           -4 $datadir/trimmed/${i}_rev_unpaired.fq.gz \
+           -r $ref1 \
+           -o $datadir/exact/$i.positions.csv \
+           -t 4) &
+    done
+fi
+
+if [ $1 == scarmag ] ; then
+    mkdir -p $datadir/exact
+
+    for i in NT350 NT351 NT358 NT359 ;
+    do
+	(echo $i
+	python ~/Code/yfan_ngs/dunlop_insert/find_exact_scars.py \
+           -1 $datadir/trimmed/${i}_fwd_paired.fq.gz \
+           -2 $datadir/trimmed/${i}_rev_paired.fq.gz \
+           -3 $datadir/trimmed/${i}_fwd_unpaired.fq.gz \
+           -4 $datadir/trimmed/${i}_rev_unpaired.fq.gz \
+           -r $ref1 \
+           -o $datadir/exact/$i.scars.csv \
+           -t 4) &
+    done
+fi
---
+++ dunlop_insert/run4/preprocess.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+rawdir=/mithril/Data/NGS/Raw/211018_dunlop_nathan
+projdir=/mithril/Data/NGS/projects/dunlop_insert
+datadir=$projdir/run4
+
+mkdir -p $datadir
+
+ref1=$projdir/refs/construct1.fa ##magnets
+ref2=$projdir/refs/construct2.fa ##ilid
+plasref=$projdir/refs/ptkei-dest-cre-loxp-sfgfp.fasta
+
+if [ $1 == trim ] ; then
+    mkdir -p $datadir/trimmed
+
+    cp ~/software/Trimmomatic-0.39/adapters/all_adapters.fa ~/Code/yfan_ngs/dunlop_insert/run4
+    
+    for i in NT350 NT351 NT358 NT359 ;
+    do
+	java -jar ~/software/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 36 -phred33 \
+	     $rawdir/${i}*_L001_R1_001.fastq.gz $rawdir/${i}*_L001_R2_001.fastq.gz \
+	     $datadir/trimmed/${i}_fwd_paired.fq.gz $datadir/trimmed/${i}_fwd_unpaired.fq.gz \
+	     $datadir/trimmed/${i}_rev_paired.fq.gz $datadir/trimmed/${i}_rev_unpaired.fq.gz \
+	     ILLUMINACLIP:all_adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:30 MINLEN:36
+    done
+fi
+
+if [ $1 == align ] ; then
+    mkdir -p $datadir/align
+
+    bwa index $ref1
+    
+    for i in NT350 NT351 NT358 NT359 ;
+    do
+	bwa mem \
+	    -t 36 \
+	    $ref1 \
+	    $datadir/trimmed/${i}_fwd_paired.fq.gz \
+	    $datadir/trimmed/${i}_rev_paired.fq.gz | \
+	    samtools view -@ 36 -b | \
+	    samtools sort -@ 36 -o $datadir/align/$i.sorted.bam
+	samtools index $datadir/align/$i.sorted.bam
+    done
+
+fi
+
+if [ $1 == yield ] ; then
+
+    for i in NT350 NT351 NT358 NT359 ;
+    do
+	bash ~/Code/utils/qc/basic_run_assess_zip.sh $rawdir/${i}*_L001_R1_001.fastq.gz >> $datadir/run4_info.csv
+	bash ~/Code/utils/qc/basic_run_assess_zip.sh $rawdir/${i}*_L001_R2_001.fastq.gz >> $datadir/run4_info.csv
+    done
+    for i in NT350 NT351 NT358 NT359 ;
+    do
+	bash ~/Code/utils/qc/basic_run_assess_zip.sh $datadir/trimmed/${i}_fwd_paired.fq.gz >> $datadir/run4_info.csv
+	bash ~/Code/utils/qc/basic_run_assess_zip.sh $datadir/trimmed/${i}_rev_paired.fq.gz >> $datadir/run4_info.csv
+    done
+
+    sed -i -e "s|$rawdir/||g" $datadir/run4_info.csv
+    sed -i -e "s|$datadir/trimmed/||g" $datadir/run4_info.csv
+    
+fi
+
